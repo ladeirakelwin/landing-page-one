87cf26fcffea90d5478a1f994c39259ef47bda0d
ladeirakelwin <ladeirakelwin@gmail.com>
2021-07-27 10:37:32 -0300

    change font size

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,8 +9,8 @@ type ButtonProps = {
 function Button(props: ButtonProps) {
   const classe =   `${props.className} ${
     props.type == "primary"
-      ? "bg-yellow-500 border-yellow-500 border-2  rounded-sm font-bold text-sm text-white"
-      : "bg-transparent border-yellow-500 border-2  rounded-sm font-bold text-sm text-yellow-500"
+      ? "bg-yellow-500 border-yellow-500 border-2  rounded-sm text-sm text-white"
+      : "bg-transparent border-yellow-500 border-2  rounded-sm text-sm text-yellow-500"
   }`
   
   return <button className={classe}>{props.children}</button>;
